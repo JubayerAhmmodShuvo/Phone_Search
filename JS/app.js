e6b6aa31566a62ccd153376c040db191a7a414e1
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-01 02:00:46 +0600

    single details with with done

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,23 @@
 document.getElementById('message').style.display = 'none';
+document.getElementById('message2').style.display = 'none';
 document.getElementById('spinner').style.display = 'none';
 const searchPhone = () => {
     document.getElementById('spinner').style.display = 'block';
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
+    if (searchText.length > 0) { /* fetch url */
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => showPhoneDetails(data.data));
+    } else {
+        document.getElementById('message2').style.display = 'block';
+        document.getElementById('spinner').style.display = 'none';
+        document.getElementById('message').style.display = 'none';
+    }
 
-    /* fetch url */
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => showPhoneDetails(data.data));
+
 
 };
 
@@ -21,24 +28,52 @@ const showPhoneDetails = (phones) => {
     if (!phones.length) {
         document.getElementById('message').style.display = 'block';
         document.getElementById('spinner').style.display = 'none';
-    }
-    phones.slice(0, 20).forEach(phone => {
+        document.getElementById('message2').style.display = 'none';
+    } else {
+        phones.slice(0, 20).forEach(phone => {
+            document.getElementById('spinner').style.display = 'none';
 
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
         <div class="card h-100 mt-5">
                     <img class="h-75 mb-3" src="${phone.image}" class="card-img-top" alt="phone image">
                     <div class="card-body">
                         <h5 class="card-title mb-2 text-center"> Name:${phone.phone_name}</h5>
                         <h5 class="card-title mb-2 text-center"> Brand:${phone.brand}</h5>
                         <div class="d-flex justify-content-end mt-2">
-                            <button class="btn btn-primary border-0 ">Details</button>
+                            <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary border-0 ">Details</button>
                         </div>
                     </div>
                 </div>
         `;
-        searchResult.appendChild(div);
-    });
+            searchResult.appendChild(div);
+        });
+    }
 
+};
+const loadPhoneDetail = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayPhoneDetails(data.data));
+
+
+};
+const displayPhoneDetails = (phone) => {
+    const details = document.getElementById('phone-details');
+    details.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img class="h-75 mb-3" src="${phone.image}" class="card-img-top" alt="phone image">
+                    <div class="card-body">
+                        <h5 class="card-title mb-2 text-center"> Name:${phone.name}</h5>
+                        <h5 class="card-title mb-2 text-center"> Brand:${phone.brand}</h5>
+                        <p></p>
+                        <p></p>
+  `;
+    details.appendChild(div);
 };
