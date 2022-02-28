09c7e7e4898199dac940621606af9e844c0430a4
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-01 03:33:02 +0600

    almost done

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,26 @@
+// message and spinner called by ID and set display to none
+
 document.getElementById('message').style.display = 'none';
 document.getElementById('message2').style.display = 'none';
 document.getElementById('spinner').style.display = 'none';
+
+// search function for search bar
+
 const searchPhone = () => {
     document.getElementById('spinner').style.display = 'block';
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+
+    // clear field
+
     searchField.value = '';
+
+    // clear details while new search
+
     const details = document.getElementById('phone-details');
     details.innerHTML = '';
-    if (searchText.length > 0) { /* fetch url */
+    if (searchText.length > 0) {
+        /* fetch url */
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         fetch(url)
             .then(response => response.json())
@@ -23,6 +35,8 @@ const searchPhone = () => {
 
 
 };
+
+// show result when search
 
 const showPhoneDetails = (phones) => {
 
@@ -55,6 +69,10 @@ const showPhoneDetails = (phones) => {
     }
 
 };
+
+// to show catch item while details button clicked
+
+
 const loadPhoneDetail = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
 
@@ -65,18 +83,32 @@ const loadPhoneDetail = id => {
 
 
 };
+
+// show details after clicking details button on
+
 const displayPhoneDetails = (phone) => {
     const details = document.getElementById('phone-details');
     details.innerHTML = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <img class="h-50 0 img-fluid mb-3" src="${phone.image}" class="card-img-top" alt="phone image">
+    <img class="h-50 0 img-fluid mb-3 " src="${phone.image}" class="card-img-top" alt="phone image">
                     <div class="card-body">
-                        <h5 class="card-title mb-2 "> Name:${phone.name}</h5>
-                        <h5 class="card-title mb-2 "> Brand:${phone.brand}</h5>
-                        <p></p>
-                        <p></p>
+                        <h5 class=" mb-2 "> Name:${phone.name}</h5>
+                        <h5 class=" mb-3 "> Brand:${phone.brand}</h5>
+                        <h6 class=" mb-4 ">Release Date: ${phone.releaseDate}</h6>
+                        <p> Storage: ${phone.mainFeatures.storage}</p>
+                        <p> Display Size: ${phone.mainFeatures.displaySize}</p>
+                        <p> ChipSet: ${phone.mainFeatures.chipSet}</p>
+                        <p> Memory: ${phone.mainFeatures.memory}</p>
+                        <p> Sensors: ${phone.mainFeatures.sensors}</p>
+                        <p> WLAN:${phone.others.WLAN}</p>
+                        <p> Memory:${phone.others.Bluetooth}</p>
+                        <p> Bluetooth:${phone.others.GPS}</p>
+                        <p> NFC:${phone.others.NFC}</p>
+                        <p> Radio:${phone.others.Radio}</p>
+                        <p> USB:${phone.others.USB}</p>
+                        
   `;
     details.appendChild(div);
 };
